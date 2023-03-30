b9c518a267bc59f3a9bae6cbfc29ae3144bf959d
Willian Dutra <contatowilliandutra@gmail.com>
2023-03-30 15:59:35 -0300

    fix: testes do endpoint Teams

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -3,7 +3,8 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
-import Teams from '../database/models/Teams';
+import ITeam from '../database/interface/ITeam';
+import TeamService from '../database/services/TeamService';
 import { allTeams, filteredTeam } from './mocks/teams.mock'
 
 import { app } from '../app';
@@ -17,14 +18,16 @@ describe('Testes unitarios do endpoint de Teams', function () {
   let chaiHttpResponse: Response
 
   it('Listando todos os times', async function () {
-    sinon.stub(Teams, 'findAll').resolves(allTeams as Teams[]);
+    sinon.stub(TeamService.prototype, 'getAllTeams')
+      .resolves(allTeams as ITeam[]);
     chaiHttpResponse = await chai.request(app).get('/teams');
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.deep.equal(allTeams);
   });
 
   it('Listando o time filtrado pelo id', async function () {
-    sinon.stub(Teams, 'findByPk').resolves(filteredTeam as Teams);
+    sinon.stub(TeamService.prototype, 'getTeamById')
+      .resolves(filteredTeam as ITeam);
     const chaiHttpResponse = await chai.request(app).get('/teams/1');
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.deep.equal(filteredTeam);
