87042b098a6b9c79a248a01c82dcabfdc5387565
Willian Dutra <contatowilliandutra@gmail.com>
2023-04-03 18:44:34 -0300

    test: iniciando testes para o endpoint leaderboard

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -3,8 +3,13 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
+import MatchesSerivice from '../database/services/MatchesService';
+import TeamService from '../database/services/TeamService';
+
 import { app } from '../app';
 import { Response } from 'superagent';
+import { allMatches, allTeams, awayLeaderboard, homeLeaderboard } from './mocks/leaderboard.mock';
+import LeaderboardService from '../database/services/LeaderboardService';
 
 chai.use(chaiHttp);
 
@@ -14,7 +19,21 @@ describe('Testes do endpoint de Leaderboard', async function () {
   let chaiHttpResponse: Response
 
   describe('Metodos GET', async function () {
+    it('Retorna uma lista com os times da casa', async function () {
+      sinon.stub(LeaderboardService.prototype, 'getLeaderboard').resolves(homeLeaderboard);
 
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/home');
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(homeLeaderboard);
+    });
+
+    it('Retorna uma lista com os times visitantes', async function () {
+      sinon.stub(LeaderboardService.prototype, 'getLeaderboard').resolves(awayLeaderboard);
+
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/away');
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(awayLeaderboard);
+    });
   })
 
   afterEach(function () {
---
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -0,0 +1,103 @@
+export const allMatches = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeamId": 9,
+    "homeTeamGoals": 1,
+    "awayTeamId": 14,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "Internacional"
+    },
+    "awayTeam": {
+      "teamName": "Santos"
+    }
+  }
+]
+
+export const homeLeaderboard = [
+  {
+    "name": "São Paulo",
+    "totalPoints": 1,
+    "totalGames": 1,
+    "totalVictories": 0,
+    "totalLosses": 0,
+    "totalDraws": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 1,
+    "goalsBalance": 0,
+    "efficiency": "50.00"
+  },
+  {
+    "name": "Internacional",
+    "totalPoints": 1,
+    "totalGames": 1,
+    "totalVictories": 0,
+    "totalLosses": 0,
+    "totalDraws": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 1,
+    "goalsBalance": 0,
+    "efficiency": "50.00"
+  }
+]
+
+export const awayLeaderboard = [
+  {
+    "name": "Grêmio",
+    "totalPoints": 1,
+    "totalGames": 1,
+    "totalVictories": 0,
+    "totalLosses": 0,
+    "totalDraws": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 1,
+    "goalsBalance": 0,
+    "efficiency": "50.00"
+  },
+  {
+    "name": "Santos",
+    "totalPoints": 1,
+    "totalGames": 1,
+    "totalVictories": 0,
+    "totalLosses": 0,
+    "totalDraws": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 1,
+    "goalsBalance": 0,
+    "efficiency": "50.00"
+  }
+]
+
+export const allTeams = [
+  {
+    "id": 8,
+    "teamName": "Grêmio"
+  },
+  {
+    "id": 9,
+    "teamName": "Internacional"
+  },
+  {
+    "id": 14,
+    "teamName": "Santos"
+  },
+  {
+    "id": 16,
+    "teamName": "São Paulo"
+  }
+]
